1e3d00c7960873d5c7b03ef7577481ec3374dd6f
ivanmaleev@inbox.ru <mudr6PHEL9$>
2021-09-26 11:11:01 +0300

    1.1. Job c параметрами [#260360]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -3,7 +3,9 @@ package ru.job4j.quartz;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.sql.*;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.*;
@@ -11,6 +13,9 @@ import static org.quartz.TriggerBuilder.*;
 import static org.quartz.SimpleScheduleBuilder.*;
 
 public class AlertRabbit {
+
+    private static Connection connection;
+
     public static void main(String[] args) {
         final Properties properties = readProperties();
         String property = properties.getProperty("rabbit.interval");
@@ -18,9 +23,19 @@ public class AlertRabbit {
             throw new IllegalStateException();
         }
         try {
+            initConnection(properties);
+        } catch (IOException | ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            return;
+        }
+        try {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
-            JobDetail job = newJob(Rabbit.class).build();
+            JobDataMap data = new JobDataMap();
+            data.put("connection", connection);
+            JobDetail job = newJob(Rabbit.class)
+                    .usingJobData(data)
+                    .build();
             SimpleScheduleBuilder times = simpleSchedule()
                     .withIntervalInSeconds(Integer.parseInt(property))
                     .repeatForever();
@@ -29,16 +44,39 @@ public class AlertRabbit {
                     .withSchedule(times)
                     .build();
             scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException se) {
+            Thread.sleep(10000);
+            scheduler.shutdown();
+        } catch (SchedulerException | InterruptedException se) {
             se.printStackTrace();
         }
 
+    }
+
+    public static void initConnection(Properties properties) throws IOException, ClassNotFoundException, SQLException {
+        InputStream in = AlertRabbit.class.getClassLoader()
+                .getResourceAsStream("rabbit.properties");
+        properties.load(in);
+        Class.forName(properties.getProperty("driver-class-name"));
+        connection = DriverManager.getConnection(
+                properties.getProperty("url"),
+                properties.getProperty("username"),
+                properties.getProperty("password"));
     }
 
     public static class Rabbit implements Job {
         @Override
         public void execute(JobExecutionContext context) throws JobExecutionException {
             System.out.println("Rabbit runs here ...");
+            Connection connection = (Connection) context.getJobDetail().getJobDataMap().get("connection");
+            try (PreparedStatement statement =
+                         connection.prepareStatement(
+                                 "insert into rabbit(created_date) values (?)",
+                                 Statement.RETURN_GENERATED_KEYS)) {
+                statement.setLong(1, System.currentTimeMillis());
+                statement.execute();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 
