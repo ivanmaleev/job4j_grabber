cb68d6e18781de6637f42ac42858d9ab782af16e
ivanmaleev@inbox.ru <mudr6PHEL9$>
2021-09-27 16:24:31 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -7,11 +7,16 @@ import org.jsoup.select.Elements;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        for (Element td : row) {
-            Element parent = td.parent();
-            System.out.println(parent.children().get(5).text());
+        String address = "https://www.sql.ru/forum/job-offers/";
+        for (int i = 1; i < 6; i++) {
+            String fullAddress = address + i;
+            Document doc = Jsoup.connect(fullAddress).get();
+            Elements row = doc.select(".postslisttopic");
+            for (Element td : row) {
+                Element parent = td.parent();
+                System.out.println(parent.children().get(5).text());
+            }
         }
+
     }
 }
