386f3eb446d5009f424d6b6e16138ef0ee6dbf59
ivanmaleev@inbox.ru <mudr6PHEL9$>
2021-09-30 09:47:32 +0300

    4. Схема таблицы Post [#1731]

--- db/scripts/init.sql
+++ db/scripts/init.sql
@@ -2,3 +2,11 @@ create table if not exists rabbit (
     id serial primary key,
     created_date bigint
 );
+
+create table if not exists post (
+    id serial primary key,
+	name varchar(255),
+	text text,	
+	link varchar(255) unique,	
+    created date
+);
